684dd356c04598650d9ec8d5e5f944be79d40c3f
reckssel <zinemil.reckssel@gmail.com>
2026-04-25 09:36:44 +0200

    Thirst try

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import socket
+from curses import panel
+import wx
 
-def check_port(host, port):
-    try:
-        whith socket.create_connection((host, port), timeout=5) as sock:
-            print(f"Port {port} is open on {host}.")
-    except Exception as e:
-        print(f"Port {port} is closed on {host}. Error: {e}")
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title="Portscanner", size=(400,200))
+
+        panel = wx.Panel(self)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        self.host = wx.TextCtrl(panel, pos=(20, 20), size=(200, 25))
+        self.port = wx.TextCtrl(panel, pos=(20, 20),size=(200, 25))
+
+        check = wx.Button(panel, label="Check", pos=(20, 60))
+        check.Bind(wx.EVT_BUTTON, self.check_port)
+    def check_port(self, event):
+        host = self.host.GetValue()
+        port = self.port.GetValue()
+        try:
+            with socket.create_connection((host, port), timeout=5) as sock:
+                print(f"Port {port} is open on {host}.")
+        except Exception as e:
+            print(f"Port {port} is closed on {host}. Error: {e}")
+
+app = wx.App()
+MyFrame()
+app.MainLoop()
